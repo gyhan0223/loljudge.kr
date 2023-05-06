eb676be4e04faa8e8e4a02ad4ada184f73e3ff8c
Eric Han <gyhan21@icloud.com>
2023-05-06 20:32:46 +0900

    third commit

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 const searchInput = document.querySelector('.search-bar input[type="text"]');
 const searchButton = document.querySelector('.search-bar button[type="submit"]');
 
+if (searchButton != null) {
 searchButton.addEventListener('click', () => {
 const searchTerm = searchInput.value.trim();
 if (searchTerm !== '') {
-    window.location.href = `https://www.op.gg/summoner/userName=${searchTerm}`;
+window.location.href = `https://www.op.gg/summoner/userName=${searchTerm}`;
 }
 });
+} else {
+console.log("Search button not found");
+}
 
 const menuLinks = document.querySelectorAll('.menu a');
 
 menuLinks.forEach((link) => {
 if (link.href === window.location.href) {
-    link.classList.add('active');
+link.classList.add('active');
 }
 });
 
@@ -23,3 +27,22 @@ document.documentElement.setAttribute('data-theme', e.target.checked ? 'dark' : 
 }
 
 toggleSwitch.addEventListener('change', switchTheme, false);
+
+const themeSwitch = document.querySelector('.theme-switch-wrapper input[type="checkbox"]');
+const themeSwitchText = document.querySelector('.theme-switch-wrapper em');
+const themeSwitchButton = document.querySelector('.theme-switch-wrapper .switch');
+
+
+themeSwitch.addEventListener('change', function(event) {
+if (event.target.checked) {
+themeSwitchText.textContent = 'Day';
+const element = document.getElementById('element-id');
+if (element) {
+element.style.backgroundColor = 'red';
+}  } else {
+themeSwitchText.textContent = 'Night';
+const element = document.getElementById('element-id');
+if (element) {
+element.parentNode.removeChild(element);
+}  }
+});
